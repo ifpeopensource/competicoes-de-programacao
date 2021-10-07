9662200260b8c3b01b384c52dbb9225831bf2a2c
Gvinfinity <bielviniciussoares@gmail.com>
2021-10-06 22:12:34 -0300

    Adiciona: "Tempo de resposta" OBI 2021

---
+++ obi/2021/tempo-de-resposta/tempo.py
@@ -0,0 +1,44 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+def main():
+    n = int(input())
+
+    dic={}
+    waitlist=[]
+
+    t=0
+
+    for i in range(n):
+        row = input().split()
+        
+     
+
+        if row[0] == "T":
+            for e in waitlist:
+                dic[e]+=int(row[1])-1
+        else:
+            for e in waitlist:
+                dic[e]+=1
+
+        if row[0] == "E":
+            waitlist.remove(row[1])
+            if not dic.get(row[1]):
+                dic[row[1]]=0   
+        if row[0] == "R":
+            waitlist.append(row[1])
+            if not dic.get(row[1]):
+                dic[row[1]]=0   
+    print("\n")
+    
+    for e in sorted(list(dic.keys())):
+        if e in waitlist:
+            print(e,"-1",sep=' ')    
+        else:
+            print(e,dic[e],sep=' ')
+
+
+
+if __name__ == "__main__":
+    main()
