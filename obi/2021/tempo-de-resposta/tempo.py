4f206d829e5558ec372e159954d53895f6e10a55
Gvinfinity <49999449+Gvinfinity@users.noreply.github.com>
2021-10-07 21:57:47 -0300

    Melhoria no espaçamento e na leitura do código
    Co-authored-by: Welton Felix <contas.weltonf@gmail.com>

--- obi/2021/tempo-de-resposta/tempo.py
+++ obi/2021/tempo-de-resposta/tempo.py
@@ -1,36 +1,36 @@
 def main():
     n = int(input())
 
-    dic={}
-    waitlist=[]
+    dic = {}
+    waitlist = []
 
-    t=0
+    t = 0
 
     for i in range(n):
         row = input().split()
 
         if row[0] == "T":
             for e in waitlist:
-                dic[e]+=int(row[1])-1
+                dic[e] += int(row[1]) - 1
         else:
             for e in waitlist:
-                dic[e]+=1
+                dic[e] += 1
 
         if row[0] == "E":
             waitlist.remove(row[1])
             if not dic.get(row[1]):
-                dic[row[1]]=0   
+                dic[row[1]] = 0   
         
         if row[0] == "R":
             waitlist.append(row[1])
             if not dic.get(row[1]):
-                dic[row[1]]=0   
+                dic[row[1]] = 0   
     
     for e in sorted(list(dic.keys())):
         if e in waitlist:
-            print(e,"-1",sep=' ')    
+            print(e, "-1", sep = ' ')    
         else:
-            print(e,dic[e],sep=' ')
+            print(e, dic[e], sep = ' ')
             
 
 if __name__ == "__main__":
